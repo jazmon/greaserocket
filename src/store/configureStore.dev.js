3cc9345abad538296fd9cbdf0c5986b547a48cda
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2017-04-14 00:28:16 +0300

    update deps

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { createStore, applyMiddleware, compose } from 'redux';
 import devTools from 'remote-redux-devtools';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { install as installLoop } from 'redux-loop';
 import { persistStore, autoRehydrate } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
