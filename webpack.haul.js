d7aa40a28a7e92979edb041df729c8bd962c063e
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2017-04-29 01:00:56 +0300

    potential HMR progress
    Not working yet, due to being in progress in haul github . com/callstack-io/haul/issues/2

--- webpack.haul.js
+++ webpack.haul.js
@@ -1,3 +1,17 @@
-module.exports = ({ platform }) => ({
-  entry: `./index.${platform}.js`,
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require('webpack');
+
+module.exports = ({ platform }, defaults) => ({
+  entry: ['react-hot-loader/patch', `./index.${platform}.js`],
+  devServer: {
+    ...defaults.webServer,
+  },
+  plugins: [
+    ...defaults.plugins,
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+  ],
 });
